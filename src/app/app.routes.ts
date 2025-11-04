83506488ae85fe5aa756dea475a4a7673a0d9a55
bw007 <mr.khasanov02@gmail.com>
2025-11-04 22:15:43 +0500

    feat: add common va main layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
+import { CommonLayout } from '@layouts/common-layout/common-layout';
 import { MainLayout } from '@layouts/main-layout/main-layout';
 
 export const routes: Routes = [
   {
     path: '',
-    component: MainLayout,
-    children: []
+    component: CommonLayout,
+    children: [
+      {
+        path: '',
+        component: MainLayout
+      }
+    ]
   }
 ];
